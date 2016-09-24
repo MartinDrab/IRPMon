4cc4aaf95cf6a42c25759192841b3682ad00d0ec
Martin Drab <martin.drab@email.cz>
2016-09-24 21:49:50 +0200

    IRPMON (GUI)  + added UAC support (the application prompts for admin rights is necessary)  + the application installs and loads the driver automatically if it is not installer or loaded  + binaries updated

--- gui/IRPMon.dpr
+++ gui/IRPMon.dpr
@@ -1,7 +1,11 @@
 program IRPMon;
 
+{$R 'uac.res' 'uac.rc'}
+
 uses
   Windows,
+  WinSvc,
+  SysUtils,
   Vcl.Forms,
   MainForm in 'MainForm.pas' {MainFrm},
   IRPMonDll in 'IRPMonDll.pas',
@@ -21,24 +25,89 @@ uses
 
 {$R *.RES}
 
+Const
+  serviceAccess = MAXIMUM_ALLOWED;
+  scmAccess = MAXIMUM_ALLOWED;
+  serviceName = 'irpmndrv';
+  serviceDescription = 'IRPMon Driver Service';
+  driverFileName = 'irpmndrv.sys';
+
 Var
+  startArgs : PWideChar;
+  hScm : THandle;
+  hService : THandle;
   err : Cardinal;
+  serviceStatus : SERVICE_STATUS;
 Begin
 Application.Initialize;
 Application.MainFormOnTaskbar := True;
 err := TablesInit('ntstatus.txt', 'ioctl.txt');
 If err = ERROR_SUCCESS Then
   begin
-  err := IRPMonDllInitialize;
-  If err = ERROR_SUCCESS Then
+  hScm := OpenSCManagerW(Nil, Nil, scmAccess);
+  If hScm <> 0 Then
     begin
-    Application.CreateForm(TMainFrm, MainFrm);
-    Application.Run;
-    IRPMonDllFinalize;
+    hService := CreateServiceW(hScm, serviceName, serviceDescription, serviceAccess, SERVICE_KERNEL_DRIVER, SERVICE_DEMAND_START, SERVICE_ERROR_NORMAL, PWideChar(ExtractFilePath(Application.ExeName) + driverFileName), Nil, Nil, Nil, Nil, Nil);
+    If hService = 0 Then
+      err := GetLastError;
+
+    If (hService <> 0) Or (err = ERROR_SERVICE_EXISTS) Or
+       (err = ERROR_SERVICE_MARKED_FOR_DELETE) Then
+      begin
+      If err = ERROR_SERVICE_EXISTS Then
+        begin
+        err := ERROR_SUCCESS;
+        hService := OpenServiceW(hScm, serviceName, serviceAccess);
+        If hService = 0 Then
+          err := GetLastError;
+        end;
+
+      If (err = ERROR_SUCCESS) Or (err = ERROR_SERVICE_MARKED_FOR_DELETE) Then
+        begin
+        err := ERROR_SUCCESS;
+        startArgs := Nil;
+        If hService <> 0 Then
+          begin
+          If Not StartService(hService, 0, startArgs) Then
+            begin
+            err := GetLastError;
+            If err = ERROR_SERVICE_ALREADY_RUNNING THen
+              err := ERROR_SUCCESS;
+            end;
+          end;
+
+        If err = ERROR_SUCCESS Then
+          begin
+          err := IRPMonDllInitialize;
+          If err = ERROR_SUCCESS Then
+            begin
+            Application.CreateForm(TMainFrm, MainFrm);
+  Application.Run;
+            IRPMonDllFinalize;
+            end
+          Else WinErrorMessage('Unable to initialize irpmondll.dll', err);
+
+          If (err <> ERROR_SUCCESS) And (hService <> 0) Then
+            ControlService(hService, SERVICE_CONTROL_STOP, serviceStatus);
+          end
+        Else WinErrorMessage('Failed to start the IRPMon driver', err);
+
+        If hService <> 0 Then
+          ;begin
+          DeleteService(hService);
+          CloseServiceHandle(hService);
+          end;
+        end
+      Else WinErrorMessage('Failed to open IRPMon driver service', err);
+      end
+    Else WinErrorMessage('Failed to create a service entry for the IRPMon driver', err);
+
+    CloseServiceHandle(hScm);
     end
-  Else WinErrorMessage('Unable to initialize irpmondll.dll', err);
+  Else WinErrorMessage('Unable to access SCM database', GetLastError);
 
   TablesFinit;
   end
 Else WinErrorMessage('Unable to initialize name tables', err);
 End.
+
