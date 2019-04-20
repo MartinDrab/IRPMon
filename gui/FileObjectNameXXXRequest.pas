a4710a38b6d6c7c965e5206045a906aa19002c03
Martin Drab <martin.drab@email.cz>
2019-04-20 10:50:36 +0200

    [gui]: GetColumnValueRaw method for file object requests

--- gui/FileObjectNameXXXRequest.pas
+++ gui/FileObjectNameXXXRequest.pas
@@ -14,12 +14,14 @@ Type
   TFileObjectNameAssignedRequest = Class (TDriverRequest)
     Public
       Constructor Create(Var ARequest:REQUEST_FILE_OBJECT_NAME_ASSIGNED); Reintroduce;
+      Function GetColumnValueRaw(AColumnType:ERequestListModelColumnType; Var AValue:Pointer; Var AValueSize:Cardinal):Boolean; Override;
       Function GetColumnValue(AColumnType:ERequestListModelColumnType; Var AResult:WideString):Boolean; Override;
     end;
 
   TFileObjectNameDeletedRequest = Class (TDriverRequest)
     Public
       Constructor Create(Var ARequest:REQUEST_FILE_OBJECT_NAME_DELETED); Reintroduce;
+      Function GetColumnValueRaw(AColumnType:ERequestListModelColumnType; Var AValue:Pointer; Var AValueSize:Cardinal):Boolean; Override;
       Function GetColumnValue(AColumnType:ERequestListModelColumnType; Var AResult:WideString):Boolean; Override;
     end;
 
@@ -45,6 +47,27 @@ CopyMemory(PWideChar(tmpFileName), fn, ARequest.FileNameLength);
 SetFileName(tmpFileName);
 end;
 
+Function TFileObjectNameAssignedRequest.GetColumnValueRaw(AColumnType:ERequestListModelColumnType; Var AValue:Pointer; Var AValueSize:Cardinal):Boolean;
+begin
+Case AColumnType Of
+  rlmctDeviceObject,
+  rlmctDeviceName,
+  rlmctResultValue,
+  rlmctResultConstant,
+  rlmctDriverObject,
+  rlmctDriverName : Result := False;
+  rlmctFileObject : begin
+    AValue := @FileObject;
+    AValueSize := SizeOf(FileObject);
+    end;
+  rlmctFileName : begin
+    AValue := PWideChar(FileName);
+    AValueSize := 0;
+    end;
+  Else Result := Inherited GetColumnValueRaw(AColumnType, AValue, AValueSize);
+  end;
+end;
+
 Function TFileObjectNameAssignedRequest.GetColumnValue(AColumnType:ERequestListModelColumnType; Var AResult:WideString):Boolean;
 begin
 Result := True;
@@ -68,6 +91,23 @@ Inherited Create(ARequest.Header);
 SetFileObject(ARequest.FileObject);
 end;
 
+Function TFileObjectNameDeletedRequest.GetColumnValueRaw(AColumnType:ERequestListModelColumnType; Var AValue:Pointer; Var AValueSize:Cardinal):Boolean;
+begin
+Case AColumnType Of
+  rlmctDeviceObject,
+  rlmctDeviceName,
+  rlmctResultValue,
+  rlmctResultConstant,
+  rlmctDriverObject,
+  rlmctDriverName : Result := False;
+  rlmctFileObject : begin
+    AValue := @FileObject;
+    AValueSize := SizeOf(FileObject);
+    end;
+  Else Result := Inherited GetColumnValueRaw(AColumnType, AValue, AValueSize);
+  end;
+end;
+
 Function TFileObjectNameDeletedRequest.GetColumnValue(AColumnType:ERequestListModelColumnType; Var AResult:WideString):Boolean;
 begin
 Result := True;
@@ -77,6 +117,7 @@ Case AColumnType Of
   rlmctResultValue,
   rlmctResultConstant,
   rlmctDriverObject,
+  rlmctFileName,
   rlmctDriverName : Result := False;
   Else Result := Inherited GetColumnValue(AColumnType, AResult);
   end;
