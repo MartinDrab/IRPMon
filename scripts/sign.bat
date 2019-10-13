71521a5e34847fa9e7135ac5f4df92ac94a09fc2
Martin Drab <martin.drab@email.cz>
2019-10-13 20:45:54 +0200

    [sign.bat]: Sign all DLLs and executables

--- scripts/sign.bat
+++ scripts/sign.bat
@@ -1,5 +1,5 @@
 set SIGNEXE="c:\Program Files (x86)\Windows Kits\10\bin\10.0.15063.0\x64\signtool.exe"
-set FILES=irpmon.exe irpmndrv.sys irpmondll.dll hexer.dll secdesc.dll pnp-ids.dll
+set FILES=irpmon.exe irpmndrv.sys kbase.dll regman.dll irpmondll.dll hexer.dll secdesc.dll pnp-ids.dll pnp-devcaps.dll pnp-interface.dll keyboard.dll
 set CROSSCERT="m:\cert\codesigning2017\crosssigning.crt"
 %SIGNEXE% sign /n "Martin Drab" /a /fd sha1 /t http://time.certum.pl /ac %CROSSCERT% /v %FILES%
 %SIGNEXE% sign /as /n "Martin Drab" /a /fd sha256 /tr http://time.certum.pl /ac %CROSSCERT% /v %FILES%
