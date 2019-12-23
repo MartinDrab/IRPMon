5e212bb1eb033bd43926188d7ac54db4b8e71c97
Martin Drab <martin.drab@email.cz>
2019-12-24 00:04:02 +0100

    [gui]: Define string constants for the File Information Class

--- gui/IRPMonDll.pas
+++ gui/IRPMonDll.pas
@@ -57,6 +57,88 @@ Const
   UNPROTECTED_DACL_SECURITY_INFORMATION      = $20000000;
   UNPROTECTED_SACL_SECURITY_INFORMATION      = $10000000;
 
+Const
+  FIleInformationClassArray : Array [0..76] Of WideString = (
+    '0',
+    'FileDirectoryInformation',
+    'FileFullDirectoryInformation',                   // 2
+    'FileBothDirectoryInformation',                   // 3
+    'FileBasicInformation',                           // 4
+    'FileStandardInformation',                        // 5
+    'FileInternalInformation',                        // 6
+    'FileEaInformation',                              // 7
+    'FileAccessInformation',                          // 8
+    'FileNameInformation',                            // 9
+    'FileRenameInformation',                          // 10
+    'FileLinkInformation',                            // 11
+    'FileNamesInformation',                           // 12
+    'FileDispositionInformation',                     // 13
+    'FilePositionInformation',                        // 14
+    'FileFullEaInformation',                          // 15
+    'FileModeInformation',                            // 16
+    'FileAlignmentInformation',                       // 17
+    'FileAllInformation',                             // 18
+    'FileAllocationInformation',                      // 19
+    'FileEndOfFileInformation',                       // 20
+    'FileAlternateNameInformation',                   // 21
+    'FileStreamInformation',                          // 22
+    'FilePipeInformation',                            // 23
+    'FilePipeLocalInformation',                       // 24
+    'FilePipeRemoteInformation',                      // 25
+    'FileMailslotQueryInformation',                   // 26
+    'FileMailslotSetInformation',                     // 27
+    'FileCompressionInformation',                     // 28
+    'FileObjectIdInformation',                        // 29
+    'FileCompletionInformation',                      // 30
+    'FileMoveClusterInformation',                     // 31
+    'FileQuotaInformation',                           // 32
+    'FileReparsePointInformation',                    // 33
+    'FileNetworkOpenInformation',                     // 34
+    'FileAttributeTagInformation',                    // 35
+    'FileTrackingInformation',                        // 36
+    'FileIdBothDirectoryInformation',                 // 37
+    'FileIdFullDirectoryInformation',                 // 38
+    'FileValidDataLengthInformation',                 // 39
+    'FileShortNameInformation',                       // 40
+    'FileIoCompletionNotificationInformation',        // 41
+    'FileIoStatusBlockRangeInformation',              // 42
+    'FileIoPriorityHintInformation',                  // 43
+    'FileSfioReserveInformation',                     // 44
+    'FileSfioVolumeInformation',                      // 45
+    'FileHardLinkInformation',                        // 46
+    'FileProcessIdsUsingFileInformation',             // 47
+    'FileNormalizedNameInformation',                  // 48
+    'FileNetworkPhysicalNameInformation',             // 49
+    'FileIdGlobalTxDirectoryInformation',             // 50
+    'FileIsRemoteDeviceInformation',                  // 51
+    'FileUnusedInformation',                          // 52
+    'FileNumaNodeInformation',                        // 53
+    'FileStandardLinkInformation',                    // 54
+    'FileRemoteProtocolInformation',                  // 55
+    'FileRenameInformationBypassAccessCheck',         // 56
+    'FileLinkInformationBypassAccessCheck',           // 57
+    'FileVolumeNameInformation',                      // 58
+    'FileIdInformation',                              // 59
+    'FileIdExtdDirectoryInformation',                 // 60
+    'FileReplaceCompletionInformation',               // 61
+    'FileHardLinkFullIdInformation',                  // 62
+    'FileIdExtdBothDirectoryInformation',             // 63
+    'FileDispositionInformationEx',                   // 64
+    'FileRenameInformationEx',                        // 65
+    'FileRenameInformationExBypassAccessCheck',       // 66
+    'FileDesiredStorageClassInformation',             // 67
+    'FileStatInformation',                            // 68
+    'FileMemoryPartitionInformation',                 // 69
+    'FileStatLxInformation',                          // 70
+    'FileCaseSensitiveInformation',                   // 71
+    'FileLinkInformationEx',                          // 72
+    'FileLinkInformationExBypassAccessCheck',         // 73
+    'FileStorageReserveIdInformation',                // 74
+    'FileCaseSensitiveInformationForceAccessCheck',   // 75
+    'FileMaximumInformation'
+  );
+
+
 Type
   BUS_QUERY_ID_TYPE = (
     BusQueryDeviceID = 0,       // <Enumerator>\<Enumerator-specific device id>
