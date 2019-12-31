4a0d8807674b7677b4b721091431559caa2d2dec
Martin Drab <martin.drab@email.cz>
2019-12-31 15:30:40 +0100

    [irpmndrv]: Add version info

---
+++ irpmndrv/version.h
@@ -0,0 +1,15 @@
+
+#ifndef __VERSION_H__
+#define __VERSION_H__
+
+
+
+#define VERR_FILE_DESCRIPTION		"IRPMon Main Kernel Driver"
+#define VERR_INTERNAL_NAME			"IRPMNDRV.SYS"
+#define VERR_ORIGINAL_FILENAME		"IRPMnDrv.sys"
+#define VERR_FILE_TYPE				VFT_DRV
+#define VERR_FILE_SUBTYPE			VFT2_UNKNOWN
+
+
+
+#endif
