b72f4aa17d0c2f98ef9faea5dadd28a44b931a5e
Martin Drab <martin.drab@email.cz>
2020-01-02 00:34:11 +0100

    [scripts]: Add installer signing script

---
+++ scripts/sign-installer.bat
@@ -0,0 +1,5 @@
+set SIGNEXE="c:\Program Files (x86)\Windows Kits\10\bin\10.0.15063.0\x64\signtool.exe"
+set FILES="..\bin\irpmon-1.0-setup.exe" 
+set CROSSCERT="m:\cert\codesigning2017\crosssigning.crt"
+%SIGNEXE% sign /n "Martin Drab" /a /fd sha1 /t http://time.certum.pl /ac %CROSSCERT% /v %FILES%
+%SIGNEXE% sign /as /n "Martin Drab" /a /fd sha256 /tr http://time.certum.pl /ac %CROSSCERT% /v %FILES%
