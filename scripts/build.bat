0673a2558f28bbe24d06534a449543e0d466388a
Martin Drab <martin.drab@email.cz>
2020-01-02 00:21:04 +0100

    [scripts]: Error handling and configuration parameter for the build script

--- scripts/build.bat
+++ scripts/build.bat
@@ -1,9 +1,18 @@
+set CONFIG=%1%
 set "VSCMD_START_DIR=%CD%"
 call "c:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvarsall.bat" x86_x64 8.1 -vcvars_ver=14.1
-MSBuild ..\irpmon.sln /Property:Configuration=Release /Property:Platform=Win32
-MSBuild ..\irpmon.sln /Property:Configuration=Release /Property:Platform=x64
+MSBuild ..\irpmon.sln /Property:Configuration=%CONFIG% /Property:Platform=Win32
+if errorlevel 1 goto failure
+MSBuild ..\irpmon.sln /Property:Configuration=%CONFIG% /Property:Platform=x64
+if errorlevel 1 goto failure
 call rsvars
-msbuild ../irpmon.groupproj /t:Build /p:config=Release /p:platform=Win32
-msbuild ../gui/irpmon.dproj /t:Build /p:config=Release /p:platform=Win64
+MSBuild ../irpmon.groupproj /t:Build /p:config=%CONFIG% /p:platform=Win32
+if errorlevel 1 goto failure
+MSBuild ../gui/irpmon.dproj /t:Build /p:config=%CONFIG% /p:platform=Win64
+if errorlevel 1 goto failure
+goto success
+:failure
+echo BUILD FAILED!
+:success
 
 
