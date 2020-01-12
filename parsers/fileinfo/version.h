91dc2c3e71dec6e16e2ca26eb4776dac0f5d15b1
Martin Drab <martin.drab@email.cz>
2020-01-12 12:49:29 +0100

    [fileinfo]: Add version information

---
+++ parsers/fileinfo/version.h
@@ -0,0 +1,15 @@
+
+#ifndef __VERSION_H__
+#define __VERSION_H__
+
+
+
+#define VERR_FILE_DESCRIPTION		"FileInformationClass Data Parser"
+#define VERR_INTERNAL_NAME			"FILEINFO.DLL"
+#define VERR_ORIGINAL_FILENAME		"FileInfo.dll"
+#define VERR_FILE_TYPE				VFT_DLL
+#define VERR_FILE_SUBTYPE			VFT2_UNKNOWN
+
+
+
+#endif
