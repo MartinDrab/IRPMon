886e99d11fd3294222c4f630959b6bc74c4cd755
Martin Drab <martin.drab@email.cz>
2020-02-20 14:10:09 +0100

    [server-svc]: Simplification and fixes

--- server-svc/server-svc.c
+++ server-svc/server-svc.c
@@ -10,8 +10,8 @@
 static SERVICE_STATUS _statusRecord;
 static SERVICE_STATUS_HANDLE _statusHandle = NULL;
 static HANDLE _exitEventHandle = NULL;
-static int _argc = 0;
-static char **_argv = NULL;
+static char *_address = NULL;
+static char *_port = NULL;
 
 
 static DWORD _ReportError(const char *Text, DWORD Code)
@@ -44,8 +44,6 @@ static DWORD WINAPI _IRPMonServiceHandlerEx(DWORD dwControl, DWORD dwEventType, 
 void WINAPI ServiceMain(DWORD argc, LPWSTR *argv)
 {
 	DWORD dwError = 0;
-	int argCount = 0;
-	char **args = NULL;
 
 	memset(&_statusRecord, 0, sizeof(_statusRecord));
 	_statusHandle = RegisterServiceCtrlHandlerExW(L"IRPMonSvc", _IRPMonServiceHandlerEx, NULL);
@@ -65,23 +63,19 @@ void WINAPI ServiceMain(DWORD argc, LPWSTR *argv)
 	}
 
 	if (_statusHandle != NULL) {
-		argCount = _argc;
-		args = _argv;
 		if (dwError == ERROR_SUCCESS) {
 			_statusRecord.dwCurrentState = SERVICE_RUNNING;
 			_statusRecord.dwControlsAccepted = SERVICE_ACCEPT_STOP;
 			SetServiceStatus(_statusHandle, &_statusRecord);
-			dwError = IRPMonServerStart(args[0], args[1], _exitEventHandle);
+			dwError = IRPMonServerStart(_address, _port, _exitEventHandle);
 			WaitForSingleObject(_exitEventHandle, INFINITE);
 			_statusRecord.dwCurrentState = SERVICE_STOPPED;
 			SetServiceStatus(_statusHandle, &_statusRecord);
 			CloseHandle(_exitEventHandle);
 		}
 
-		if (_statusHandle != NULL) {
-			_statusRecord.dwCurrentState = SERVICE_STOPPED;
-			SetServiceStatus(_statusHandle, &_statusRecord);
-		}
+		_statusRecord.dwCurrentState = SERVICE_STOPPED;
+		SetServiceStatus(_statusHandle, &_statusRecord);
 	}
 
 	return;
@@ -93,15 +87,27 @@ int main(int argc, char *argv[])
 	int ret = 0;
 	SERVICE_TABLE_ENTRYW svcTable[2];
 
+	switch (argc) {
+		case 2:
+			_address = strdup(argv[1]);
+			if (_address == NULL)
+				ret = ENOMEM;
+		case 3:
+			if (ret == 0) {
+				_port = strdup(argv[2]);
+				if (_port == 0)
+					ret = ENOMEM;
+			}
+			break;
+	}
+
 	if (argc == 3) {
-		_argc = argc - 1;
-		_argv = argv + 1;
 		memset(svcTable, 0, sizeof(svcTable));
 		svcTable[0].lpServiceName = L"IRPMonSvc";
 		svcTable[0].lpServiceProc = ServiceMain;
 		if (!StartServiceCtrlDispatcherW(svcTable))
 			ret = GetLastError();
-	} else ret = ERROR_INVALID_PARAMETER;
+	}
 
 	return ret;
 }
