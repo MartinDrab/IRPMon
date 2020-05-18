fbeaa3fd4a7612fd52fd6fcc07a7f82290a09567
Martin Drab <martin.drab@email.cz>
2020-05-18 21:12:50 +0100

    [server-app]: Start the IRPMon driver on server startup

--- irpmon-server/server.c
+++ irpmon-server/server.c
@@ -14,10 +14,28 @@
 int main(int argc, char **argv)
 {
 	int ret = 0;
+	wchar_t* tmp = NULL;
+	wchar_t fileName[MAX_PATH];
 
 	if (argc != 3) {
 		ret = -1;
 		fprintf(stderr, "Usage: %s <host> <port>\n", argv[0]);
+		goto Exit;
+	}
+
+	memset(fileName, 0, sizeof(fileName));
+	GetModuleFileNameW(NULL, fileName, sizeof(fileName) / sizeof(fileName[0]));
+	tmp = fileName + wcslen(fileName);
+	while (tmp != fileName && *tmp != L'\\')
+		--tmp;
+
+	if (*tmp == L'\\')
+		++tmp;
+
+	wcscpy(tmp, L"irpmndrv.sys");
+	ret = IRPMonServerStartDriver(fileName, FALSE);
+	if (ret != ERROR_SUCCESS) {
+		fprintf(stderr, "[ERROR]: IRPMonServerStartDriver: %u\n", ret);
 		goto Exit;
 	}
 
