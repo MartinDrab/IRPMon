e719179207fb0b18188f523c367974a1e5bc573e
Martin Drab <martin.drab@email.cz>
2021-01-10 23:54:48 +0100

    [libreqlistdll]: Import reqlist.dll from Delphi to C/C++

---
+++ include/reqlist.h
@@ -0,0 +1,43 @@
+
+#ifndef __REQLIST_H__
+#define __REQLIST_H__
+
+#include <windows.h>
+#include "general-types.h"
+
+
+typedef enum _ERequestListObjectType {
+	rlotDriver,
+	rlotDevice,
+	rlotFile,
+	rlotProcess,
+} ERequestListObjectType, *PERequestListObjectType;
+
+
+typedef void (cdecl REQUEST_LIST_CALLBACK)(PREQUEST_HEADER Request, void *Context, BOOLEAN Store);
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+DWORD ReqListCreate(PHANDLE List);
+void ReqListFree(HANDLE List);
+void ReqListAssignParserList(HANDLE List, HANDLE Parsers);
+DWORD ReqListAdd(HANDLE List, PREQUEST_GENERAL Request);
+void ReqListClear(HANDLE List);
+DWORD ReqListGetObjectName(HANDLE List, void *Object, ERequestListObjectType Type, wchar_t **Name);
+void ReqListFreeObjectName(wchar_t *Name);
+DWORD ReqListSetCallback(HANDLE List, REQUEST_LIST_CALLBACK *Routine, void *Context);
+void ReqListUnregisterCallback(HANDLE List);
+DWORD ReqListSave(HANDLE List, ERequestLogFormat Format, const wchar_t *FileName);
+DWORD ReqListLoad(HANDLE List, const wchar_t *FileName);
+
+DWORD ReqListModuleInit(const wchar_t *LibraryName);
+void ReqListModuleFinit(void);
+
+#ifdef __cplusplus
+}
+#endif
+
+
+#endif
