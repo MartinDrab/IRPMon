0cd23c8ef5c6ac24cf04cd2528ba6bccf4c41fb5
Martin Drab <martin.drab@email.cz>
2021-01-18 21:22:04 +0100

    [irpmonc]: Add version information

---
+++ irpmonc/version.h
@@ -0,0 +1,15 @@
+
+#ifndef __VERSION_H__
+#define __VERSION_H__
+
+
+
+#define VERR_FILE_DESCRIPTION		"IRPMon Command-Line Interface"
+#define VERR_INTERNAL_NAME			"IRPMONC.EXE"
+#define VERR_ORIGINAL_FILENAME		"irpcmonc.exe"
+#define VERR_FILE_TYPE				VFT_APP
+#define VERR_FILE_SUBTYPE			VFT2_UNKNOWN
+
+
+
+#endif
